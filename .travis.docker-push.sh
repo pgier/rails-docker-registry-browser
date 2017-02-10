feb1d3811f2e039a49336f8d8dd2e704aee97290
Paul Gier <pgier@redhat.com>
2017-02-10 00:10:14 -0600

    Tag image before pushing

--- .travis.docker-push.sh
+++ .travis.docker-push.sh
@@ -1,8 +1,12 @@
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 if [ "$TRAVIS_TAG" ]; then
-    docker push ${DOCKER_IMAGE_NAME}:${TRAVIS_TAG};
+    DOCKER_TAG="${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}"
+    docker tag "${DOCKER_IMAGE_NAME}:latest" "${DOCKER_TAG}"
+    docker push "${DOCKER_TAG}"
 else
-    docker push ${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT::8};
+    DOCKER_TAG="${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT::8}"
+    docker tag "${DOCKER_IMAGE_NAME}:latest" "${DOCKER_TAG}"
+    docker push "${DOCKER_TAG}"
 fi
 
